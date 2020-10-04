57ec5368fc0deb7037205feae1e3d8626f016b04
Markus <68151874+toastering@users.noreply.github.com>
2020-10-03 19:19:52 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace realsteamcmd
+{
+    class Program
+    {
+        public static void Listen()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Steam>");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            string input = Console.ReadLine();
+            string[] split = input.Split(' ');
+            switch (split[0])
+            {
+                case "help":
+                    if (split.Length > 1)
+                    {
+                        Console.WriteLine($@"No specific help topic found for '{split[1]}', returning 'find' results. . .
+ConVars:
+Commands:
+");
+                        return;
+                    }
+                    Console.WriteLine(Strings.helpmsg);
+                    break;
+
+                case "quit":
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine($"Command not found: {input}\n");
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Strings.intro);
+            for(; ; )
+            {
+                Listen();
+            }
+        }
+    }
+}
---
+++ Strings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace realsteamcmd
+{
+    class Strings
+    {
+        public static string intro = @"Looks like steam didn't shutdown cleanly, scheduling immediate update check
+[  0%] Checking for available updates...
+[----] Verifying installation...
+Steam Console Client (c) Valve Corporation
+-- type 'quit' to exit --
+Loading Steam API...OK.
+";
+        public static string helpmsg = @"
+Usage:  steamcmd ["" + COMMAND[ARG]...""]...
+  or:   steamcmd +runscript SCRIPTFILE
+
+Help topics - type ""help <topic>"" or run with ""--help <topic>"" for more information:
+
+               login : Logging in to Steam
+             scripts : Executing a sequence of commands via a script file
+         commandline : Executing commands directly via the OS command line
+             convars : Options and settings that affect this program session
+           app_build : Building Steam application content (only for licensed Steam application developers)
+          app_update : Installing/updating a Steam application on the local filesystem (EG dedicated servers)
+
+
+You can also type ""find <string>"" to see a list of all commands and convars that contain or reference<string>.
+
+";
+    }
+}
